f15148ad00fe5c97f7dc4c1b1024528deee8763e
kn211_bmo <kn211_bmo@student.ztu.edu.ua>
2023-08-26 17:28:19 +0300

    initializated project + created basic layout

---
+++ components/shared/LeftSideBar.tsx
@@ -0,0 +1,9 @@
+const LeftSideBar = () => {
+  return (
+    <aside className="sticky left-0 w-1/5 h-screen border-r-2 border-gray-200">
+      <div></div>
+    </aside>
+  );
+};
+
+export default LeftSideBar;
---
+++ components/shared/RightSideBar.tsx
@@ -0,0 +1,9 @@
+const RightSideBar = () => {
+  return (
+    <aside className="sticky right-0 w-1/5 h-screen border-r-2 border-gray-200">
+      <div></div>
+    </aside>
+  );
+};
+
+export default RightSideBar;
